9d64ead785e44bf7c27b01da45e2ccf619ea16e1
PradeepShetty14 <pradeepshetty_14@yahoo.co.in>
2020-06-18 12:23:14 +0530

    Update numeric-constant.go

--- numeric-constant.go
+++ numeric-constant.go
@@ -2,21 +2,14 @@ package main
 
 import "fmt"
 
-const (
-	// Create a huge number by shifting a 1 bit left 100 places.
-	// In other words, the binary number that is 1 followed by 100 zeroes.
-	Big = 1 << 100
-	// Shift it right again 99 places, so we end up with 1<<1, or 2.
-	Small = Big >> 99
-)
-
-func needInt(x int) int { return x*10 + 1 }
-func needFloat(x float64) float64 {
-	return x * 0.1
+func factorial(i int)int {
+   if(i <= 1) {
+      return 1
+   }
+   return i * factorial(i - 1)
 }
-
-func main() {
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+func main() { 
+   var i int = 15
+   var j int =20
+   fmt.Printf("Factorial of %d is %d", i, factorial(i))
 }
